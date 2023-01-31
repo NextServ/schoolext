9e8b71468e326500fb488d8983201124157b9768
mincerray1 <johnrobertanguluan@gmail.com>
2023-02-01 02:16:17 +0800

    dp

---
+++ schoolext/school_extension/doctype/dragonpay_settings/dragonpay_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, SERVIO Enterprise and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class DragonPaySettings(Document):
+	pass
