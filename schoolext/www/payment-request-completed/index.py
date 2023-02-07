4c6ac5dfa03809973913ea31216ad61016618853
mincerray1 <johnrobertanguluan@gmail.com>
2023-02-08 03:33:41 +0800

    error log

--- schoolext/www/payment-request-completed/index.py
+++ schoolext/www/payment-request-completed/index.py
@@ -45,7 +45,7 @@ def get_context(context):
     generated_digest = hashlib.sha1(sha1_input.encode())
 
     if digest != generated_digest:
-        frappe.log_error("dp_returnurl_params message {0} Invalid digest {1} generated digest {2}".format(message, digest, generated_digest))
+        frappe.log_error(message="dp_returnurl_params message {0} Invalid digest {1} generated digest {2}".format(message, digest, generated_digest), title="dp_returnurl_params message {0} Invalid digest".format(txnid))
     else:
         pass
 
