544dadc5e8bec77f4bb0c2f00726bab1644ea81b
mincerray1 <johnrobertanguluan@gmail.com>
2023-03-13 16:52:40 +0800

    remove alerts

--- schoolext/public/js/schoolext_web.js
+++ schoolext/public/js/schoolext_web.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
-    alert('test');
     hide_manage_3rd_party_apps();
     function hide_manage_3rd_party_apps() {
-        alert('hello');
         let manage_third_party_apps = $('div.account-info:last-child');
         manage_third_party_apps.addClass('d-none');
     }
